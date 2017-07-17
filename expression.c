0a49a461d0e15f13850f4935fda56ae3b691118a
radong <756445638@qq.com>
2017-07-17 19:22:31 +0800

    fix js.y

---
+++ expression.c
@@ -0,0 +1,11 @@
+#include "js.h"
+
+
+int get_expression_list_length(ExpressionList* list){
+    int length = 0;
+    while(NULL != list){
+        list = list->next;
+        length++;
+    }
+    return length;
+};
